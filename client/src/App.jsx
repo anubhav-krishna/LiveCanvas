694305316417272b85ef9a17192be4006959b843
Anubhav Krishna <anubhavkrishna2010@gmail.com>
2026-01-29 23:46:45 +0530

    Added drawing functionality to canvas with mouse events

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,51 @@
 import { useEffect, useRef } from "react";
-import { setupCanvas } from "./canvas";
+import { setupCanvas , startStroke , endStroke , addPoint } from "./canvas";
 
 function App() {
   const canvasRef = useRef(null);
+  const contextRef = useRef(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
-    setupCanvas(canvas);
+     const ctx= setupCanvas(canvas);
+     contextRef.current = ctx;
+
+   const getMousePosition = (event)=>{
+     const rect = canvas.getBoundingClientRect();
+     return {
+       x: event.clientX - rect.left,
+       y: event.clientY - rect.top
+     };
+   };
+
+    const onMouseDown = (event) => {
+      const {x,y} = getMousePosition(event);
+      startStroke(x,y);
+    };
+
+    const onMouseMove = (event) => {
+      if (!contextRef.current) return;
+
+      const {x,y} = getMousePosition(event);
+      addPoint(x,y,contextRef.current);
+    };
+
+    const onMouseUp = () => {
+      endStroke();
+    };
+
+    canvas.addEventListener("mousedown", onMouseDown);
+    canvas.addEventListener("mousemove", onMouseMove);
+    window.addEventListener("mouseup", onMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", onMouseDown);
+      canvas.removeEventListener("mousemove", onMouseMove);
+      window.removeEventListener("mouseup", onMouseUp);
+    };
+
   }, []);
 
   return <canvas ref={canvasRef} />;
--- client/src/canvas.js
+++ client/src/canvas.js
@@ -11,3 +11,39 @@ export function setupCanvas(canvas) {
 
   return ctx;
 }
+
+let isDrawing = false;
+let activeStroke = null;
+
+export function startStroke(x,y){
+    isDrawing= true;
+
+    activeStroke= {
+        color : "white",
+        width: 2,
+        points: [{x,y}]
+    };
+}
+
+export function addPoint(x,y,ctx){
+    if(!isDrawing || !activeStroke)return;
+
+    const points= activeStroke.points;
+    const prevPoint= points[points.length -1];
+
+    ctx.strokeStyle= activeStroke.color;
+    ctx.lineWidth= activeStroke.width;
+    ctx.lineCap= "round";
+    
+    ctx.beginPath();
+    ctx.moveTo(prevPoint.x,prevPoint.y);
+    ctx.lineTo(x,y);
+    ctx.stroke();
+
+    points.push({x,y});
+}
+
+export function endStroke(){
+    isDrawing= false;
+    activeStroke=null;
+}
