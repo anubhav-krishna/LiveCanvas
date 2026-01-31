b3a2febf50af9a43a6e0505aa297f8415db13dd1
Anubhav Krishna <anubhavkrishna2010@gmail.com>
2026-02-01 00:14:57 +0530

    Fixing bugs

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["https://live-canvas-8wwadstr7-anubhav-krishnas-projects.vercel.app/","http://localhost:5173"],
+    origin: ["https://live-canvas-n4m5rdyvw-anubhav-krishnas-projects.vercel.app","http://localhost:5173"],
     methods: ["GET", "POST"]
   }
 });
