67eb0b144e0b85169407713fc0a0949f12d076b9
Anubhav Krishna <anubhavkrishna2010@gmail.com>
2026-01-31 23:57:19 +0530

    fix: remove unnecessary credentials option from CORS settings

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173"],
-    methods: ["GET", "POST"],
-    credentials: true
+    methods: ["GET", "POST"]
   }
 });
  io.on("connection", (socket) => {
