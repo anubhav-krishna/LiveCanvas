04a12e051734dbf0a6c90b90dead748db69e68fa
Anubhav Krishna <anubhavkrishna2010@gmail.com>
2026-02-01 00:10:08 +0530

    fix: update CORS settings to allow requests from the production URL

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: ["https://live-canvas-8wwadstr7-anubhav-krishnas-projects.vercel.app/","http://localhost:5173"],
     methods: ["GET", "POST"]
   }
 });
